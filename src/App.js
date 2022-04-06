bafe7c1095a3b778d2fd3f9697d7a9ebd3665061
Sumitha-HVA <sumithav.1rn17is102@gmail.com>
2022-04-06 14:36:12 +0530

    Frontend, commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{Component} from 'react';
+import { Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const data =[
+  {
+    Bus_Name :" KSRTC ",
+    Src : " K.G.F ",
+    Dest : "Bengaluru",
+    Tickets : " 25 ",
+    Tickets_avail :" 20",
+    Date : " 30/3/2022 ",
+    Time : "5:30 pm"
+  },
+
+  {
+    Bus_Name :" Prakruthi Travels",
+    Src : " Bengaluru ",
+    Dest : " Sakleshpur",
+    Tickets : " 25 ",
+    Tickets_avail :" 10",
+    Date : " 3/4/2022 ",
+    Time : "6:00 am"
+  },
+
+  {
+    Bus_Name :" Neha Travels",
+    Src : " Bengaluru ",
+    Dest : " Mysore",
+    Tickets : " 20 ",
+    Tickets_avail :" 15",
+    Date : " 5/4/2022 ",
+    Time : "8:30 am"
+  }
+
+]
+
+function App () {
+      return(
+          <>
+              <nav className="main-nav">
+                  {/*website-Name */}
+                  <div className="website-name">
+                      <h2> BUS TICKET BOOKING </h2> 
+                  </div>
+              </nav>
+            <table>
+              <thead>
+                <tr>
+                  <th>Bus_Name</th>
+                  <th>Src</th>
+                  <th>Dest</th>
+                  <th>Tickets_avail</th>
+                  <th>Tickets</th>
+                  <th>Date</th>
+                  <th>Time</th>
+                </tr>
+                {data.map((val, key) => {
+                  return (
+                    <tr key={key}>
+                      <td>{val.Bus_Name}</td>
+                      <td>{val.Src}</td>
+                      <td>{val.Dest}</td>
+                      <td>{val.Tickets_avail}</td>
+                      <td>{val.Tickets}</td>
+                      <td>{val.Date}</td>
+                      <td>{val.Time}</td>
+                    </tr>
+                  )
+                })}
+              </thead>
+            </table>
+              
+          </>
+      );
+  
 }
+
 
 export default App;
